6fc5960dc6bdb3d6dce399e18897434b6ac34e32
KimKim Horas <kimkim4@gmail.com>
2020-06-10 14:53:04 -0700

    Completed user-can flips-cards

--- memory_match-master/assets/js/script.js
+++ memory_match-master/assets/js/script.js
@@ -1,1 +1,12 @@
 
+var mainCardClick = document.querySelector('#gameCards');
+mainCardClick.addEventListener('click', handleClick);
+
+function handleClick(event){
+  // console.log(event);
+  if(event.target.className.indexOf('card-back') === -1){
+    return;
+  }
+  event.target.classList.add('hidden');
+
+}
