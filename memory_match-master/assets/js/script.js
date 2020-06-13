414cf5ea1aa777bfc556ec9cd7286d2658ac1729
KimKim Horas <kimkim4@gmail.com>
2020-06-13 13:06:37 -0700

    Completed user-can-view-stats

--- memory_match-master/assets/js/script.js
+++ memory_match-master/assets/js/script.js
@@ -1,4 +1,3 @@
-
 var mainGameCardClick = document.querySelector('#gameCards');
 mainGameCardClick.addEventListener('click', handleClick);
 
@@ -11,13 +10,17 @@ var secondCardClasses = null;
 var matches = 0;
 var maxMatches = 9;
 
+var attempts = 0;
+var gamesPlayed = 0;
+
+
 function handleClick(event){
   if(event.target.className.indexOf('card-back') === -1){
     return;
   }
   event.target.classList.add('hidden');
 
- //--- properly stire the card references when tow cards clicked: ----------
+ //--- properly store the card references when two cards clicked: ----------
  //1. check if no cards are being clicked then store the firstCardClick
  //2. else: if the firstCardClicked is true; store the secondCardClicked
   if(firstCardClicked === null){
@@ -29,6 +32,9 @@ function handleClick(event){
     secondCardClicked = event.target;
     secondCardClasses = secondCardClicked.previousElementSibling.className;
     mainGameCardClick.removeEventListener('click', handleClick); // preventing clicks more than 2 cards
+    attempts++;
+    // console.log("attempts:", attempts)
+    displayStats();
 
     //-------- compare if it has the same class/not -------
     if (firstCardClasses === secondCardClasses) {
@@ -38,6 +44,8 @@ function handleClick(event){
         secondCardClicked = null;
         matches++;
         // console.log("matches variable:", matches)
+        displayStats();
+
         if(maxMatches === matches){
           // console.log("you have won!")
           document.querySelector(".modal").classList.remove("hidden")
@@ -51,7 +59,19 @@ function handleClick(event){
         mainGameCardClick.addEventListener('click', handleClick);
         firstCardClicked = null;
         secondCardClicked = null;
+        displayStats();
       }, 1500)
     }
   }
+}
+
+
+function displayStats(){
+  document.getElementById("gamesPlayedResult").textContent =  gamesPlayed;
+  document.getElementById("attemptsResult").textContent =  attempts;
+  document.getElementById("accuracyResult").textContent = calculateAccuracy(attempts,matches);
+}
+
+function calculateAccuracy(attempts, matches){
+  return Math.trunc((matches/ attempts) * 100) + " %";
 }
